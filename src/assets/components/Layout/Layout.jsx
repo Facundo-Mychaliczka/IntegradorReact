bfa72cd0d89432ac1c6398a10e8a46af625dd8bf
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2023-10-12 12:18:33 -0300

    FixedErrorLayout

--- src/assets/components/Layout/Layout.jsx
+++ src/assets/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import LayoutWrapper from './LayoutsTYLES'
+import LayoutWrapper from './LayoutStyles'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { useLocation } from 'react-router-dom'
