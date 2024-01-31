c7384c3d546bd8b703e6999437e96486cc0fa141
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-01-31 19:40:42 -0300

    fixedBuyError

--- src/assets/components/Form/FormikData/FormikData.js
+++ src/assets/components/Form/FormikData/FormikData.js
@@ -20,11 +20,8 @@ export const validationSchema = Yup.object({
         .required("Este campo es obligatorio."),
     adress: Yup.string("Este campo sólo requiere el nombre de calle.")
         .required("Este campo es obligatorio."),
-    location : Yup.number("este campo sólo requiere números.")
+    location : Yup.string("este campo sólo requiere números.")
         .required("Este campo es obligarotio."),
-    email: Yup.string()
-        .email("Este campo sólo requiere un e-mail")
-        .required("Este campo es obligatorio"),
     cellphone: Yup.string()
         .matches(cellphoneValidation, "Este campo requiere de 10 números.")
         .required("Este campo es obligatorio.")
