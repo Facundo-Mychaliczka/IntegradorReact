105dbcd04ba921080cade0aa5ff59cf3976fc1fc
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-01-31 21:01:04 -0300

    persist

--- src/assets/FormRegister/FormRegister.jsx
+++ src/assets/FormRegister/FormRegister.jsx
@@ -26,6 +26,7 @@ const FormRegister = () => {
                 if (user) {
                     dispatch(setCurrentUser({
                         ...user.user,
+                        token: user.token
                     }))
                 }
             }}
