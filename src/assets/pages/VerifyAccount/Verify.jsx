42f49b9e7cdbc8d830522eb1d97742c9bf55a5cd
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-01 00:29:33 -0300

    page

--- src/assets/pages/VerifyAccount/Verify.jsx
+++ src/assets/pages/VerifyAccount/Verify.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { VerifyBox, VerifyWrapper } from './VerifyStyles'
+import VerifyForm from '../../components/VerifyForm/VerifyForm'
 
 const Verify = () => {
   return (
     <VerifyWrapper>
       <VerifyBox>
-        
+        <VerifyForm/>
       </VerifyBox>
     </VerifyWrapper>
   )
