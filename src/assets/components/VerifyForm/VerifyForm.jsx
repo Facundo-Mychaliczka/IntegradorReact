449746aca4bd2fe7c08e536faaa91e536df3d6f2
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-01 21:00:00 -0300

    vP8

--- src/assets/components/VerifyForm/VerifyForm.jsx
+++ src/assets/components/VerifyForm/VerifyForm.jsx
@@ -2,15 +2,10 @@ import { Formik } from 'formik'
 import React from 'react'
 import {StyledFormValidationCode} from "./VerifyFormStyles"
 import InputFormVerify from './Input/InputFormVerify'
-import { INITIAL_VALUES_VERIFY,ValidationSchemaVerify } from './FormikData/FormikDataVerify'
-import {verifyAccount} from "../../../axios/axiosVerify.js"
 import SubmitButtonVerify from "./Button/SubmitButtonVerify"
-import { useNavigate } from "react-router-dom"
+
 
 const VerifyForm = () => {
-
-  const navigate = useNavigate()
-
   return (
     <Formik
     >
--- src/axios/axiosVerify.js
+++ src/axios/axiosVerify.js
@@ -1,17 +1,17 @@
-import axios from "axios"
+// import axios from "axios"
 
-const BASE_URL = 'https://api-back-facundo-mychaliczka.vercel.app/'
+// const BASE_URL = 'https://api-back-facundo-mychaliczka.vercel.app/'
 
-export const verifyAccount = async (email, code)=> {
+// export const verifyAccount = async (email, code)=> {
 
-    try {
-        const response = await axios.patch(`${BASE_URL}auth/verify`, {
-            email,
-            code
-        })
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        return alert (error.response.data.msg)
-    }
-}
+//     try {
+//         const response = await axios.patch(`${BASE_URL}auth/verify`, {
+//             email,
+//             code
+//         })
+//         console.log(response.data);
+//         return response.data
+//     } catch (error) {
+//         return alert (error.response.data.msg)
+//     }
+// }
