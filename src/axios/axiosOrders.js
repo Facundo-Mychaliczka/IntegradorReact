cac18e89744499ee9138b88da5006ccf6b48f36c
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-06 19:27:22 -0300

    undo

--- src/axios/axiosOrders.js
+++ src/axios/axiosOrders.js
@@ -12,7 +12,7 @@ export const postOrder = async (order, currentUser)=> {
         })
         return console.log(response);
     } catch (error) {
-        return console.log(error);
+        return alert (error.response.msg)
     }
 }
 
