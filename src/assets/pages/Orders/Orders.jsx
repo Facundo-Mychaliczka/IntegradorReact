cd7e0888f17dc638b3664ad93f6b06b57f711eda
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-06 23:04:49 -0300

    pruebaSinAsyncAwait

--- src/assets/pages/Orders/Orders.jsx
+++ src/assets/pages/Orders/Orders.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 
 const Orders =  () => {
   const currentUser = useSelector((state) => state.user.currentUser)
-   const orders = async () => await getOrders(currentUser)
+   const orders =  getOrders(currentUser)
    console.log(orders);
   return (
     <OrdersWrapper>
