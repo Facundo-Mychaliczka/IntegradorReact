bcf62ecedf21d2b561193da54eba97d0adf14557
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-06 23:00:12 -0300

    logAOrdersData

--- src/assets/pages/Orders/Orders.jsx
+++ src/assets/pages/Orders/Orders.jsx
@@ -7,13 +7,10 @@ const Orders =  () => {
   const currentUser = useSelector((state) => state.user.currentUser)
    const orders = async () => await getOrders(currentUser)
    const ordersData = orders.data
+   console.log(ordersData);
   return (
     <OrdersWrapper>
-        {
-          ordersData.map((order) => {
-            return <OrderContainer>{order}</OrderContainer>
-          })
-        }
+        
     </OrdersWrapper>
   )
 }
