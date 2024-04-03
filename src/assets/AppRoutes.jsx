dfb5e3e0cbbd22f7ab2f7804329622bccb154708
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-04-03 15:44:57 -0300

    addedVercelAnalytics

--- src/assets/AppRoutes.jsx
+++ src/assets/AppRoutes.jsx
@@ -1,57 +1,50 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Home from './pages/Home/Home'
-import ProductsPage from './pages/Products/Products'
-import Error from './pages/Error/Error'
-import Login from './pages/Login/Login'
-import Membership from './pages/Membership/Membership'
-import BuyPage from './pages/SuccessBuy/BuyPage'
-import Register from './pages/Register/Register'
-import UserProfile from './pages/User/UserProfile'
-import Verify from "./pages/VerifyAccount/Verify"
-import ProtectedRoute from '../assets/components/ProtectedRoute/ProtectedRoute'
-import Orders from './pages/Orders/Orders'
-
-
-
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import ProductsPage from "./pages/Products/Products";
+import Error from "./pages/Error/Error";
+import Login from "./pages/Login/Login";
+import Membership from "./pages/Membership/Membership";
+import BuyPage from "./pages/SuccessBuy/BuyPage";
+import Register from "./pages/Register/Register";
+import UserProfile from "./pages/User/UserProfile";
+import Verify from "./pages/VerifyAccount/Verify";
+import ProtectedRoute from "../assets/components/ProtectedRoute/ProtectedRoute";
+import Orders from "./pages/Orders/Orders";
+import { Analytics } from "@vercel/analytics/react";
 
 const AppRoutes = () => {
+	return (
+		<>
+			<BrowserRouter>
+				<Layout>
+					<Analytics />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/products" element={<ProductsPage />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/membership" element={<Membership />} />
+						<Route path="/user" element={<UserProfile />} />
+						<Route path="/verify" element={<Verify />} />
+						<Route path="/myOrders" element={<Orders />} />
 
-  return (
-    
-    <>
-    <BrowserRouter>
-      <Layout >
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/products' element={<ProductsPage/>}/>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/membership' element={<Membership/>}/>
-          <Route path='/user' element={<UserProfile/>}/>
-          <Route path='/verify' element={<Verify/>}/>
-          <Route path='/myOrders' element={<Orders/>}/>
-          
+						<Route
+							path="/BuyPage"
+							element={
+								<ProtectedRoute redirectTo="/login">
+									<BuyPage />
+								</ProtectedRoute>
+							}
+						/>
 
+						<Route path="*" element={<Error />} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
+		</>
+	);
+};
 
-          <Route
-          path='/BuyPage' 
-          element = {
-            <ProtectedRoute redirectTo="/login">
-              <BuyPage/>
-            </ProtectedRoute>
-          }
-          />
-          
-          <Route path='*' element={<Error/>}/>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-    </>
-  )
-}
-
-export default AppRoutes
+export default AppRoutes;
